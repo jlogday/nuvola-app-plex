b47978c2bbf5bc3e5e7ea804f3b6d4975beaef24
Jason Day <jason@jlogday.com>
2015-06-08 14:15:25 -0400

    Add skeleton integration script

--- integrate.js
+++ integrate.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Jiří Janoušek <janousek.jiri@gmail.com>
+ * Copyright 2015 Jason Day <jason@jlogday.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met: 
@@ -34,39 +34,8 @@ var player = Nuvola.$object(Nuvola.MediaPlayer);
 var PlaybackState = Nuvola.PlaybackState;
 var PlayerAction = Nuvola.PlayerAction;
 
-// Translations
-var _ = Nuvola.Translate.gettext;
-var C_ = Nuvola.Translate.pgettext;
-
-// Constants
-var ADDRESS = "app.address";
-var ADDRESS_DEFAULT = "default";
-var ADDRESS_CUSTOM = "custom";
-var HOST = "app.host";
-var PORT = "app.port";
-var COUNTRY_VARIANT = "app.country_variant";
-var COUNTRY_VARIANTS = [
-    ["de", C_("Amazon variant", "Germany")],
-    ["fr", C_("Amazon variant", "France")],
-    ["co.uk", C_("Amazon variant", "United Kingdom")],
-    ["com", C_("Amazon variant", "United States")]
-];
-
 // Create new WebApp prototype
 var WebApp = Nuvola.$WebApp();
-
-WebApp._onInitAppRunner = function(emitter)
-{
-    Nuvola.WebApp._onInitAppRunner.call(this, emitter);
-    
-    Nuvola.config.setDefault(ADDRESS, ADDRESS_DEFAULT);
-    Nuvola.config.setDefault(HOST, "");
-    Nuvola.config.setDefault(PORT, "");
-    Nuvola.config.setDefault(COUNTRY_VARIANT, "com");
-    
-    Nuvola.core.connect("InitializationForm", this);
-    Nuvola.core.connect("PreferencesForm", this);
-}
 
 // Initialization routines
 WebApp._onInitWebWorker = function(emitter)
@@ -74,13 +43,12 @@ WebApp._onInitWebWorker = function(emitter)
     Nuvola.WebApp._onInitWebWorker.call(this, emitter);
 
     var state = document.readyState;
-    if (state === "interactive" || state === "complete")
+    if (state === "interactive" || state === "complete") {
         this._onPageReady();
-    else
+    }
+    else {
         document.addEventListener("DOMContentLoaded", this._onPageReady.bind(this));
-    
-    this.testPrototypes();
-    this.testTranslation();
+    }
 }
 
 // Page is ready for magic
@@ -91,106 +59,20 @@ WebApp._onPageReady = function()
 
     // Start update routine
     this.update();
-    
-    Nuvola.global._config_set_object = function()
-    {
-        var track = {
-        artist: "Jane Bobo",
-        album: "Best hits",
-        title: "How I met you"
-        }
-        Nuvola.config.set("integration.track", track);
-        console.log(Nuvola.config.get("integration.track"));
-    }
 }
 
 // Extract data from the web page
 WebApp.update = function()
 {
     var track = {
+        title: null,
+        artist: null,
+        album: null,
         artLocation: null // always null
     }
 
-    var idMap = {title: "track", artist: "artist", album: "album"}
-    for (var key in idMap)
-    {
-        try
-        {
-            track[key] = document.getElementById(idMap[key]).innerText || null;
-        }
-        catch(e)
-        {
-            // Always expect errors, e.g. document.getElementById() might return null
-            track[key] = null;
-        }
-    }
-
     player.setTrack(track);
-    
-    try
-    {
-        switch(document.getElementById("status").innerText)
-        {
-            case "Playing":
-                var state = PlaybackState.PLAYING;
-                break;
-            case "Paused":
-                var state = PlaybackState.PAUSED;
-                break;
-            default:
-                var state = PlaybackState.UNKNOWN;
-                break;
-        }
-    }
-    catch(e)
-    {
-        // Always expect errors, e.g. document.getElementById("status") might be null
-        var state = PlaybackState.UNKNOWN;
-    }
-
-    player.setPlaybackState(state);
-    
-    var enabled;
-    try
-    {
-        enabled = !document.getElementById("prev").disabled;
-    }
-    catch(e)
-    {
-        enabled = false;
-    }
-    player.setCanGoPrev(enabled);
-
-    try
-    {
-        enabled  = !document.getElementById("next").disabled;
-    }
-    catch(e)
-    {
-        enabled = false;
-    }
-    player.setCanGoNext(enabled);
-
-    var playPause = document.getElementById("pp");
-    try
-    {
-        enabled  = playPause.innerText == "Play";
-    }
-    catch(e)
-    {
-        enabled = false;
-    }
-    player.setCanPlay(enabled);
-
-    try
-    {
-        enabled  = playPause.innerText == "Pause";
-    }
-    catch(e)
-    {
-        enabled = false;
-    }
-    player.setCanPause(enabled);
+    player.setPlaybackState(PlaybackState.UNKNOWN);
     
     // Schedule the next update
     setTimeout(this.update.bind(this), 500);
@@ -199,143 +81,6 @@ WebApp.update = function()
 // Handler of playback actions
 WebApp._onActionActivated = function(emitter, name, param)
 {
-    switch (name)
-    {
-    case PlayerAction.TOGGLE_PLAY:
-    case PlayerAction.PLAY:
-    case PlayerAction.PAUSE:
-    case PlayerAction.STOP:
-        Nuvola.clickOnElement(document.getElementById("pp"));
-        break;
-    case PlayerAction.PREV_SONG:
-        Nuvola.clickOnElement(document.getElementById("prev"));
-        break;
-    case PlayerAction.NEXT_SONG:
-        Nuvola.clickOnElement(document.getElementById("next"));
-        break;
-    }
-}
-
-WebApp._onInitializationForm = function(emitter, values, entries)
-{
-    if (!Nuvola.config.hasKey(ADDRESS))
-        this.appendPreferences(values, entries);
-}
-
-WebApp._onPreferencesForm = function(emitter, values, entries)
-{
-    this.appendPreferences(values, entries);
-}
-
-WebApp.appendPreferences = function(values, entries)
-{
-    values[ADDRESS] = Nuvola.config.get(ADDRESS);
-    values[HOST] = Nuvola.config.get(HOST);
-    values[PORT] = Nuvola.config.get(PORT);
-    entries.push(["header", _("Logitech Media Server")]);
-    entries.push(["label", _("Address of your Logitech Media Server")]);
-    entries.push(["option", ADDRESS, ADDRESS_DEFAULT, _("use default address ('localhost:9000')"), null, [HOST, PORT]]);
-    entries.push(["option", ADDRESS, ADDRESS_CUSTOM, _("use custom address"), [HOST, PORT], null]);
-    entries.push(["string", HOST, "Host"]);
-    entries.push(["string", PORT, "Port"]);
-    
-    values[COUNTRY_VARIANT] = Nuvola.config.get(COUNTRY_VARIANT);
-    entries.push(["header", _("Amazon Cloud Player")]);
-    entries.push(["label", _("Preferred national variant")]);
-    for (var i = 0; i < COUNTRY_VARIANTS.length; i++)
-        entries.push(["option", COUNTRY_VARIANT, COUNTRY_VARIANTS[i][0], COUNTRY_VARIANTS[i][1]]);
-}
-
-WebApp.testPrototypes = function()
-{
-    var Building = Nuvola.$prototype(null);
-
-    Building.$init = function(address)
-    {
-            this.address = address;
-    }
-    
-    Building.printAddress = function()
-    {
-        console.log(this.address);
-    }
-    
-    var Shop = Nuvola.$prototype(Building);
-    
-    Shop.$init = function(address, goods)
-    {
-            Building.$init.call(this, address)
-            this.goods = goods;
-    }
-    
-    Shop.printGoods = function()
-    {
-            console.log(this.goods);
-    }
-    
-    var house = Nuvola.$object(Building, "King Street 1024, London");
-    house.printAddress();
-    
-    var candyShop = Nuvola.$object(Shop, "King Street 1024, London", "candies");
-    candyShop.printAddress();
-    candyShop.printGoods();
-}
-
-WebApp.testTranslation = function()
-{
-    var _ = Nuvola.Translate.gettext;
-    
-    /// You can use tree slashes to add comment for translators.
-    /// It has to be on a line preceding the translated string though.
-    console.log(_("Hello world!"));
-    var name = "Jiří";
-    /// {1} will be replaced by name
-    console.log(Nuvola.format(_("Hello {1}!"), name));
-    
-    var ngettext = Nuvola.Translate.ngettext;
-    var eggs = 5;
-    var text = ngettext(
-        /// You can use tree slashes to add comment for translators.
-        /// It has to be on a line preceding the singular string though.
-        /// {1} will be replaced by number of eggs in both forms,
-        /// but can be omitted as shown in singular form.
-        "There is one egg in the fridge.",
-        "There are {1} eggs in the fridge.",
-        eggs);
-    console.log(Nuvola.format(text, eggs));
-    var eggs = 1;
-    var text = ngettext(
-        "There is one egg in the fridge.",
-        "There are {1} eggs in the fridge.",
-        eggs);
-    console.log(Nuvola.format(text, eggs));
-    
-    var C_ = Nuvola.Translate.pgettext;
-    
-    /// You can use tree slashes to add comment for translators.
-    /// It has to be on a line preceding the translated string though.
-    console.log(C_("Navigation", "Back"));
-    console.log(C_("Body part", "Back"));
-    
-    console.log(Nuvola.Translate.gettext("Bye World!"));
-    var name = "Jiří";
-    console.log(Nuvola.format(Nuvola.Translate.gettext("Bye {1}!"), name));
-    
-    var eggs = 5;
-    var text = Nuvola.Translate.ngettext(
-        "There is one child in the fridge.",
-        "There are {1} children in the fridge.",
-        eggs);
-    console.log(Nuvola.format(text, eggs));
-    var eggs = 1;
-    var text = Nuvola.Translate.ngettext(
-        "There is one child in the fridge!",
-        "There are {1} children in the fridge!",
-        eggs);
-    console.log(Nuvola.format(text, eggs));
-    
-    console.log(Nuvola.Translate.pgettext("Navigation", "Forward"));
-    console.log(Nuvola.Translate.pgettext("Body part", "Forward"));
 }
 
 WebApp.start();
